760ed769c95d34d53fe64c6e6d282e3455199acc
Malcolm Ford <malcolmf38@gmail.com>
2017-12-19 23:01:22 -0600

    I have started on the navbar search functionality

--- app/controllers/nav_controller.js
+++ app/controllers/nav_controller.js
@@ -1,5 +1,14 @@
 'use strict';
 
 app.controller('nav_controller', function ($scope, $routeParams, database) {
+  // This is pulling a list of all technologies, and setting them equal to a variable
+  const search_results = function () {
+    database.get_technologies()
+      .then((data) => {
+        $scope.search_return = data.data.data;
+        console.log('navbar search results', $scope.search_return);
+      });
+  };
 
+  search_results();
 });
