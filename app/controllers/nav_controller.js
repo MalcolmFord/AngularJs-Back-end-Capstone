8a0acc7aee882fb4658ebc5933b42b3be1fa66d9
Malcolm Ford <malcolmf38@gmail.com>
2017-12-31 23:10:05 -0600

    I have the basic logic set up

--- app/controllers/nav_controller.js
+++ app/controllers/nav_controller.js
@@ -2,28 +2,29 @@
 
 app.controller('nav_controller', function ($scope, $routeParams, database, $window) {
   // GLOBAL VARIABLE
-  var current_user = "";
+  $scope.current_user = "";
 
   // This function gets the cookie data, and sets the current_user variable to the user id
   const get_user = function () {
     let a = database.get_cookies();
-    console.log('a', a.user_id);
-    current_user = a.user_id;
-    console.log('b', current_user);
+    // console.log('a', a.user_id);
+    $scope.current_user = a.user_id;
+    // console.log('b', $scope.current_user);
 
   };
   // This logic checks to see if the variable "current_user" is empty
-  $scope.validate = function () {
-    if (current_user == "") {
-      return false;
-      $scope.$apply;
-    }
-    else {
+  $scope.validate = function (a) {
+    if (a === "")
       return true;
-      $scope.$apply;
-    };
+    // $scope.$apply;
+    else
+      return false;
+    // $scope.$apply;
+
   };
-  console.log('c', $scope.validate());
+  console.log('c', $scope.validate($scope.current_user));
+  console.log('d', $scope.current_user);
+
 
 
   get_user();
